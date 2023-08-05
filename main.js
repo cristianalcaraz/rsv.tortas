74ab5d03093f38fcb85e9559b0b418b098a88fe0
cristianalcaraz <134733926+cristianalcaraz@users.noreply.github.com>
2023-08-05 12:18:18 -0300

    new page

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// Rerva tu torta 
+
+function saludar() {
+    let nombre = prompt("Ingrese el nombre");
+    alert(`Hola ${nombre}`);
+}
+
+saludar();
+
+
+let sabor = parseInt(prompt("Ingrese el numero de sabor")); //variable
+
+if (sabor >= 5) { //condicionar
+    alert("Gracias por su pedido");
+} else {
+    alert("No disponible");
+}
+
+function intenteNuevamente() { //funcion
+    let intente = prompt("Intente nuevamente diga SI ");
+    alert(`si${si}`);
+}
+intenteNuevamente();
